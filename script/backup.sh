acd706971d97aff8de057ac990e4bca5edaf8281
Kento210 <kentokentokento913@gmail.com>
2024-04-19 23:18:56 +0900

    update: コンテナの停止と始動を追加

--- script/backup.sh
+++ script/backup.sh
@@ -6,7 +6,18 @@ source .env
 # ファイル名
 ARCHIVE_NAME="minecraft_server_backup_$(date +%Y-%m-%d)"
 
+# Dockerコンテナを停止
+echo "Stopping Docker containers..."
+sudo docker-compose -f /home/MinecraftServer/docker/docker-compose.yml stop
+
 # バックアップを取得する
+echo "Taking backup..."
 scp -r -P ${PORT} -i ${KEY_PASS} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR} ${LOCAL_DIR}
 
 echo "Backup completed successfully."
+
+# Dockerコンテナを開始
+echo "Starting Docker containers..."
+sudo docker-compose -f /home/MinecraftServer/docker/docker-compose.yml start
+
+echo "Docker containers started successfully."
