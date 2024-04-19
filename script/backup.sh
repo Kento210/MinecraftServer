dfa6b0406f365128e913edf52aedb7c066595401
Kento210 <kentokentokento913@gmail.com>
2024-04-19 21:26:25 +0900

    fix: envで読み込む予定のものを消していなかった

--- script/backup.sh
+++ script/backup.sh
@@ -3,14 +3,8 @@
 # .envファイルを読み込む
 source .env
 
-# マインクラフトサーバーのデータがあるリモートディレクトリ
-REMOTE_DIR="/home/kento/test_bk"
-
-# 圧縮されたファイルの名前
+# ファイル名
 ARCHIVE_NAME="minecraft_server_backup_$(date +%Y-%m-%d)"
-
-# ローカルで圧縮ファイルを保存するディレクトリ
-LOCAL_DIR="./backup"
 
 # バックアップを取得する
 scp -r -P ${PORT} -i ${KEY_PASS} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR} ${LOCAL_DIR}
